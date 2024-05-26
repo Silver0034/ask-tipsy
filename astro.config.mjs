86fef42bf762382f03514872bb69d8c6d026a771
Jacob Lodes <contact@jlodes.com>
2024-05-25 23:50:18 -0400

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import { defineConfig } from 'astro/config'
 
+import cloudflare from '@astrojs/cloudflare'
+
 // https://astro.build/config
 export default defineConfig({
 	output: 'hybrid',
 	redirects: {
 		'/categories': '/recipes'
+	},
+	adapter: cloudflare({
+		imageService: 'compile'
+	}),
+	image: {
+		entrypoint: 'compile',
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'external-image-host-domain.com'
+			}
+		]
 	}
 })
