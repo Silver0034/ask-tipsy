57395bee28aeb7fd0a30500c77ae0f89784ec3fc
Jacob Lodes <contact@jlodes.com>
2024-12-26 20:10:17 -0500

    fix npm vulnerability & update font packages

--- scripts/font-fallbacks.js
+++ scripts/font-fallbacks.js
@@ -6,8 +6,9 @@ import roboto from '@capsizecss/metrics/roboto'
 import { fromFile } from '@capsizecss/unpack'
 
 const fonts = {
-	Montserrat:
-		'./node_modules/@fontsource/montserrat/files/montserrat-latin-700-normal.woff'
+	Kreon: './node_modules/@fontsource/kreon/files/kreon-latin-700-normal.woff',
+	Truculenta:
+		'./node_modules/@fontsource/truculenta/files/truculenta-latin-400-normal.woff'
 }
 
 async function generateFontFace(fontName, fontPath) {
